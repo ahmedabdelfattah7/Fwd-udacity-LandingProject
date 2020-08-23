fb7e1786b2bbd809418c8390fcf3b92266d9113d
stark-af1 <65439051+stark-af1@users.noreply.github.com>
2020-08-23 13:02:09 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,41 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+let t1=performance.now();
+let navList = document.querySelector('#navbar__list');
+let sections = document.querySelectorAll('section');
+
+// build the nav
+for(let section of sections){
+    let navItem = document.createElement('li');
+    navItem.innerHTML = `<a class = "menu__link" href = "#${section.id}">${section.id}</a>`;
+    navList.appendChild(navItem);
+}
+
+// Add class 'active' to section when near top of viewport
+document.addEventListener('scroll',()=>{
+    let mini = Math.min(Math.abs(sections[0].getBoundingClientRect().top),Math.abs(sections[1].getBoundingClientRect().top),Math.abs(sections[2].getBoundingClientRect().top),Math.abs(sections[3].getBoundingClientRect().top));
+    sections.forEach(sec=>{
+        sec.getBoundingClientRect().top==mini?sec.classList.add('your-active-class'):sec.classList.remove('your-active-class');
+    });    
+});
+
+// Scroll to anchor ID using scrollTO event
+document.querySelectorAll('.menu__link').forEach(link=>{
+    link.addEventListener('click',function (e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({behavior: 'smooth'});
+    });
+});
