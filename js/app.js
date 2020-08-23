7f856d85fb30ac0d8278cded8259e7fd354a2416
stark-af1 <65439051+stark-af1@users.noreply.github.com>
2020-08-23 14:52:00 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -17,14 +17,14 @@ let p1=performance.now();
 let navLists = document.querySelector('#navbar__list');
 let sections = document.querySelectorAll('section');
 
-// build the nav
+// build the navigation
 for(let item of sections){
     let navItem = document.createElement('li');
     navItem.innerHTML = `<a class = "menu__link" href = "#${item.id}">${item.id}</a>`;
     navLists.appendChild(navItem);
 }
 
-// Add class 'active' to section when near top of viewport
+// Add class 'active' to section 
 document.addEventListener('scroll',()=>{
     let mini = Math.min(Math.abs(sections[0].getBoundingClientRect().top),Math.abs(sections[1].getBoundingClientRect().top),Math.abs(sections[2].getBoundingClientRect().top),Math.abs(sections[3].getBoundingClientRect().top));
     sections.forEach(sec=>{
@@ -32,7 +32,7 @@ document.addEventListener('scroll',()=>{
     });    
 });
 
-// Scroll to anchor ID using scrollTO event
+// Scroll to anchor ID using scroll event
 document.querySelectorAll('.menu__link').forEach(link=>{
     link.addEventListener('click',function (e){
         e.preventDefault();
